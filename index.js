ab3f3edadec362d9b7828164ff94867b3bd05808
bsihame <sihamebazi@pursuit.org>
2020-01-23 11:29:51 -0500

    week 2 problem 3

--- index.js
+++ index.js
@@ -182,6 +182,32 @@ function list(names){
   console.log(list([ {name: 'Bart'}, {name: 'Lisa'} ]))
 
 
+//=================================================================================
+console.log("problem 3");
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let sum = 0;
+    for (var i in arr) {
+      if (arr[i] > 0) {
+        sum += arr[i];
+      }
+    };
+    return sum;
+  }
+console.log(positiveSum([1,2,3,4,5]))
+console.log(positiveSum([1,-2,3,4,5]))
+console.log(positiveSum([]))
+console.log(positiveSum([-1,-2,-3,-4,-5]))
+console.log(positiveSum([-1,2,3,4,-5]))
+//=================================================================================
+console.log("problem 4");
+  
   
 
 
